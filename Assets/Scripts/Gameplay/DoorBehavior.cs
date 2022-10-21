cdf98098e2b5af0036ac24f35d8eb469cc923685
Woodrow Crawford <WoodrowCrawford03@gmail.com>
2022-10-21 17:11:37 -0500

    Added door mechanic; will refine later

--- Assets/Scripts/Gameplay/DoorBehavior.cs
+++ Assets/Scripts/Gameplay/DoorBehavior.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 
 public class DoorBehavior : MonoBehaviour
 {
 
     [SerializeField] private GameObject _door;
-    [SerializeField] private float _doorOpenSpeed;
+
+
 
 
     private void Update()
     {
-        
-       
+     
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ghoul") 
+        {
+            _door.transform.Rotate(new Vector3(0, -90, 0));
+            Debug.Log("enemy is by the door");
+        }
+    }
 }
