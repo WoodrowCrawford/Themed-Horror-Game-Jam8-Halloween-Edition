0d06c85617afb76007ebb874f0f39135b55ded39
Woodrow Crawford <woodrowcrawford03@gmail.com>
2023-01-13 09:41:36 -0600

    Working on the clown mechanic

---
+++ Assets/Scripts/Gameplay/JackInTheBoxBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class JackInTheBoxBehavior : MonoBehaviour
+{ 
+
+}
+
+
+
+
+
